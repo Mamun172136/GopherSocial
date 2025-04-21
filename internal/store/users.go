e000bdd88969b0862209394352ea5e2343b3a422
Mamun172136 <ma1536204@gmail.com>
2025-04-21 15:40:32 +0600

    add Create method to insert new users into the database

--- internal/store/users.go
+++ internal/store/users.go
@@ -5,10 +5,46 @@ import (
 	"database/sql"
 )
 
+type User struct {
+	ID        int64    `json:"id"`
+	Username  string   `json:"username"`
+	Email     string   `json:"email"`
+	Password  string `json:"-"`
+	CreatedAt string   `json:"created_at"`
+	
+}
+
 type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore)Create (ctx context.Context) error{
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO posts (username,  password, email) VALUES (&1,&2, &3, &4) RETURNING id, created_at, updated_at
+	`
+
+	// ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	// defer cancel()
+
+	
+
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		
+			return err
+		}
+	
+
 	return nil
 }
